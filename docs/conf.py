56324e4f031dccfcc45cf1b904f5c3b0189f7465
= <burrscurr@users.noreply.github.com>
2021-01-24 18:13:10 +0100

    Bump version to 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020-2021, burrscurr'
 author = 'burrscurr'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
